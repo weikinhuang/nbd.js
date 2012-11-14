061a4bd495c225d5ed946610bdd37f12cb158d5e
Alex Lee <aintaer@gmail.com>
2012-11-13 19:36:55 -0500

    Including a global declaration for non-AMD

---
+++ nbd.js
@@ -0,0 +1,37 @@
+/*jslint evil:true */
+require([
+        'nbd/Class',
+        'nbd/Model',
+        'nbd/View',
+        'nbd/Controller',
+        'nbd/Events',
+        'nbd/traits/pubsub',
+        'nbd/utils/async',
+        'nbd/utils/pipe',
+        'nbd/utils/jxon'
+], function(Class, Model, View, Controller, pubsub, async, pipe, jxon) {
+  'use strict';
+
+  /**
+   * @see http://stackoverflow.com/questions/3277182/how-to-get-the-global-object-in-javascript 
+   */
+  var global = (new Function('return this'))(),
+
+  exports = {
+    Class : Class,
+    Model : Model,
+    View : View,
+    Controller : Controller,
+    traits : {
+      pubsub : pubsub
+    },
+    utils : {
+      async : async,
+      pipe : pipe,
+      jxon : jxon
+    }
+  };
+
+  global.nbd = exports;
+
+});
