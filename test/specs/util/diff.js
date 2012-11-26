14914f5a5b89dc1c6a2342790b86621cd0225c03
Aintaer <aintaer@gmail.com>
2012-11-26 15:16:19 -0500

    Stub test for util/diff

---
+++ test/specs/util/diff.js
@@ -0,0 +1,9 @@
+/*global jasmine, describe, it, expect, runs, waitsFor */
+define(['real/util/diff'], function(diff) {
+  'use strict';
+
+  describe('util/diff', function() {});
+
+  return diff;
+
+});
