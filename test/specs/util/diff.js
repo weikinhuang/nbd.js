52b29e6349e09e31ad7c06765d63c89e838d829d
Alex Lee <aintaer@aintaer.com>
2012-11-28 18:26:57 -0500

    redo of diff algorithm and its tests

--- test/specs/util/diff.js
+++ test/specs/util/diff.js
@@ -2,7 +2,71 @@
 define(['real/util/diff'], function(diff) {
   'use strict';
 
-  describe('util/diff', function() {});
+  describe('util/diff', function() {
+    it('is a function', function() {
+      expect(diff).toEqual(jasmine.any(Function));
+    });
+
+    it('returns an empty object with no differences', function() {
+      var o, p, r;
+
+      o = {simple: "foobar"};
+      p = {simple: "foobar"};
+      r = diff(o,p);
+
+      expect(r).toEqual(jasmine.any(Object));
+      expect(Object.keys(r).length).toBe(0);
+    });
+    
+    it('returns an array of differences', function() {
+      var o, p, r;
+
+      o = {simple: "foobar"};
+      p = {simple: "foobaz"};
+      r = diff(o,p);
+
+      expect(r).toEqual(jasmine.any(Object));
+      expect(r.simple).toBeDefined();
+      expect(r.simple[0]).toBe("foobar");
+      expect(r.simple[1]).toBe("foobaz");
+    });
+
+    it('calls a callback for every difference', function() {
+      var o, p, r, cb = jasmine.createSpy('diffspy');
+
+      o = {
+        unchanged:10,
+        simple: "foobar",
+        complex: {
+          meaning: 42
+        }
+      };
+      p = {
+        unchanged:10,
+        simple: "foobaz",
+        complex: {
+          meaning: '42'
+        }
+      };
+      r = diff(o, p, cb);
+
+      expect(cb).toHaveBeenCalledWith('simple','foobar','foobaz');
+      expect(cb).toHaveBeenCalledWith('complex',o.complex,p.complex);
+      expect(cb.callCount).toBe(2);
+    });
+
+    it('does not infinitely recurse', function() {
+      var r,
+      o = {},
+      p = {};
+
+      o.self = o;
+      p.self = o;
+      r = diff(o,p);
+      expect(r).toEqual(jasmine.any(Object));
+      expect(Object.keys(r).length).toBe(0);
+    });
+  });
 
   return diff;
 
--- util/diff.js
+++ util/diff.js
@@ -3,7 +3,37 @@ define(['nbd/util/extend'], function(extend) {
 
   var stack = [];
 
-  return function diff(cur, prev, callback, deep) {
+  function objectCheck(cur, prev) {
+    var key, equal=true;
+
+    for (key in cur) {
+      if (cur[key] !== prev[key]) {
+        return false;
+      }
+
+      if (cur[key] && cur.hasOwnProperty(key) && typeof cur[key] === "object") {
+        // Property has been visited, skip
+        if (stack.indexOf(cur[key]) >= 0 ) { continue; }
+
+        try {
+          stack.push(cur[key]);
+
+          // Recurse into object to find diff
+          equal = equal && objectCheck(prev[key], cur[key]);
+        }
+        catch (emptyArgs) {}
+        finally {
+          stack.pop();
+        }
+      }
+
+      if (!equal) { return equal; }
+    }
+
+    return equal;
+  }
+
+  return function diff(cur, prev, callback) {
     var key, difference, differences = {};
 
     if (typeof prev !== "object" || typeof cur !== "object" ||
@@ -16,26 +46,19 @@ define(['nbd/util/extend'], function(extend) {
 
     for (key in cur) {
       if (cur.hasOwnProperty(key)) {
-        // Property has been visited, skip
-        if (stack.inArray(cur[key]) >= 0 ) { continue; }
-
-        if (prev[key] !== cur[key]) {
+        if (prev[key] !== cur[key] ||
+            (typeof cur[key] === "object" && 
+             cur[key] && 
+             !objectCheck(cur[key], prev[key])
+            )
+           ) 
+        {
           differences[key] = [cur[key], prev[key]];
           if (callback) {
             callback.apply(this, [key, cur[key], prev[key]]);
           }
         }
-        else if (deep && typeof cur[key] === "object" && cur[key]) {
-          try {
-            // Recurse into object to find diff
-            stack.push(cur[key]);
-            diff.call(this, prev[key], cur[key], callback);
-          }
-          catch (emptyArgs) {}
-          finally {
-            stack.pop();
-          }
-        }
+
         delete prev[key];
       }
     }
