77ff41e67881790b0d3e78b1cfe402321d703f00
Alex Lee <aintaer@aintaer.com>
2012-11-30 18:45:27 -0500

    Tests for views including jquery.tmpl trait

--- test/specs/trait/jquery.tmpl.js
+++ test/specs/trait/jquery.tmpl.js
@@ -1,6 +1,30 @@
-/*global jasmine, describe, it, expect, spyOn, loadFixtures */
-define(['real/trait/jquery.tmpl'], function(jqtmpl) {
+/*global jasmine, describe, xdescribe, it, xit, expect, spyOn, beforeEach */
+define(['real/trait/jquery.tmpl', 'jquery', 'nbd/View'], function(jqtmpl, $, nView) {
   'use strict';
+
+  var View = nView.extend(),
+  instance;
+  View.mixin(jqtmpl);
+
+  beforeEach(function() {
+    instance = new View();
+  });
+
+  describe('View.prototype.templateScript', function() {
+    it('finds the class template from an instance', function() {
+      var instance = new View();
+
+      expect(function(){
+        instance.templateScript();
+      }).toThrow();
+      expect( instance.templateScript(false) ).toBe(false);
+
+      $('<script type="text/x-jquery-tmpl" id="mytest">Hello</script>').appendTo(document.body);
+      View.TEMPLATE_ID = "mytest";
+      expect( instance.templateScript(false) ).not.toBe(false);
+      expect( instance.templateScript().html() ).toEqual('Hello');
+    });
+  });
 
   return jqtmpl;
 });
