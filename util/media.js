793e60a84000d1b47a84b3378593879d13d9c957
Alex Lee <aintaer@gmail.com>
2012-12-13 15:32:24 -0500

    Media query utility module

---
+++ util/media.js
@@ -0,0 +1,52 @@
+/**
+ * Responsive media query callbacks
+ * @see https://developer.mozilla.org/en-US/docs/DOM/Using_media_queries_from_code
+ */
+define(['nbd/util/extend', 'nbd/trait/pubsub'], function(extend, pubsub) {
+  'use strict';
+
+  var mqChange, mediaCheck,
+  matchMedia = window.matchMedia || window.msMatchMedia;
+
+  function bindMedia( breakpoint, query ) {
+    var match = window.matchMedia( query );
+    match.addListener( mqChange.bind(match, breakpoint) );
+    if (match.matches) { mqChange.call(match, breakpoint); }
+  }
+
+  mediaCheck = function media( options, query ) {
+    var breakpoint;
+
+    // No matchMedia support
+    if ( !matchMedia ) {
+      throw new Error('Media queries not supported.');
+    }
+
+    // Has matchMedia support
+    if ( typeof options === 'string' ) {
+      bindMedia( options, query );
+      return media;
+    }
+
+    if ( typeof options === 'object' ) {
+      for (breakpoint in options) {
+        if (options.hasOwnProperty(breakpoint)) {
+          query = options[breakpoint];
+          bindMedia( breakpoint, query );
+        }
+      }
+    }
+    return media;
+
+  };
+
+  extend(mediaCheck, pubsub);
+
+  mqChange = function(breakpoint) {
+    mediaCheck.trigger(breakpoint + this.matches ? ':enter' : ':exit');
+    mediaCheck.trigger(breakpoint);
+  };
+
+  return mediaCheck;
+
+});
