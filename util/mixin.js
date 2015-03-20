ad96cf0167cedfe81cd627528a6b621b53d48011
Alex Lee <aintaer@aintaer.com>
2015-03-20 00:20:44 -0400

    util/mixin: rewritten in es6

--- util/mixin.js
+++ util/mixin.js
@@ -1,13 +1,7 @@
-/* istanbul ignore if */
-if (typeof define !== 'function') { var define = require('amdefine')(module); }
-define(function() {
-  'use strict';
-
-  return function mixin(target, abstract) {
-    var descriptor = {};
-    Object.keys(abstract).forEach(function(prop) {
-      descriptor[prop] = Object.getOwnPropertyDescriptor(abstract, prop);
-    });
-    Object.defineProperties(target, descriptor);
-  };
-});
+export default function mixin(target, abstract) {
+  const descriptors = {};
+  for (let prop of Object.keys(abstract)) {
+    descriptors[prop] = Object.getOwnPropertyDescriptor(abstract, prop);
+  }
+  Object.defineProperties(target, descriptors);
+}
