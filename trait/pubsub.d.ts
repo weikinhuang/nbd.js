2551d6b1e40d9371b88c12711c5dd5128c9c534d
Wei Kin Huang <wehuang@adobe.com>
2019-03-28 16:16:03 -0400

    Types: Update PubSubTrait on/one/off to be optional args

--- trait/pubsub.d.ts
+++ trait/pubsub.d.ts
@@ -1,9 +1,9 @@
 export type evtCallback = (...args: any[]) => any;
 
 export interface PubSubTrait {
-  on(event: string, callback: evtCallback, context: any): this;
-  one(event: string, callback: evtCallback, context: any): this;
-  off(event: string, callback: evtCallback, context: any): this;
+  on(event: string, callback: evtCallback, context?: any): this;
+  one(event: string, callback: evtCallback, context?: any): this;
+  off(event: string, callback?: evtCallback, context?: any): this;
   trigger(event: string, ...args: any[]): this;
   listenTo(object: PubSubTrait, events: string | { [event: string]: evtCallback }, callback?: evtCallback): this;
   listenOnce(object: PubSubTrait, events: string | { [event: string]: evtCallback }, callback?: evtCallback): this;
